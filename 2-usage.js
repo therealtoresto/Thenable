6bf46d877448dafa3bfc1f4971b5e1123ee307a3
TORESTO <therealtoresto@gmail.com>
2020-07-09 15:38:34 +0300

    thenable obj example

---
+++ 2-usage.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const getPerson = id => {
+    const thenable = {
+        then(onFulfilled) {
+            setTimeout(() => {
+                const person = { id, name: 'Marcus Aurelius' };
+                onFulfilled(person);
+            }, 1000);
+        }
+    };
+    return thenable;
+};
+
+(async () => {
+    const person = await getPerson(10);
+    console.dir({ person });
+})();
